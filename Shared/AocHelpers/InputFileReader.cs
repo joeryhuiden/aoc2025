117e7555a2d9002987b71db6255c524d935a0e11
joeryhuiden <joeryhuiden@users.noreply.github.com>
2025-12-01 09:44:37 -0500

    Setup before starting day 1

---
+++ Shared/AocHelpers/InputFileReader.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AocHelpers;
+
+
+public class InputFileReader
+{
+    public static string ReadInputFile(string relativePath)
+    {
+        // Get the absolute path of the current working directory
+        string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+        // Combine the base directory and the relative path into a full path
+        string fullPath = Path.Combine(currentDirectory, relativePath);
+
+        // Ensure the file is copied to the output directory in Visual Studio
+        // by setting the file's 'Copy to Output Directory' property to 'Copy if newer'.
+
+        try
+        {
+            // Read the file using the constructed full path.
+            return File.ReadAllText(fullPath);
+        }
+        catch
+        {
+            Console.WriteLine($"Error while reading file at: {fullPath}");
+            throw;
+        }
+    }
+
+    public static T? ReadInputFileToObject<T>(string relativePath)
+    {
+        var jsonString = ReadInputFile(relativePath);
+
+        try
+        {
+            return JsonConvert.DeserializeObject<T>(jsonString);
+        }
+        catch
+        {
+            Console.WriteLine($"Error while converting to object");
+            throw;
+        }
+    }
+}
---
+++ Solutions/20251201/Program.cs
@@ -0,0 +1,7 @@
+﻿using AocHelpers;
+
+Console.WriteLine("Hello, World!");
+
+InputFileReader.ReadInputFile("./input.txt");
+
+Console.WriteLine("Done!");
