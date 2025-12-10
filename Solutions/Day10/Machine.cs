bf1941809e3b32b552d26ea4fae110b7906d4598
joeryhuiden <joeryhuiden@users.noreply.github.com>
2025-12-10 13:04:16 -0500

    Day10

---
+++ Solutions/Day10/Machine.cs
@@ -0,0 +1,154 @@
+﻿namespace Day10;
+
+public class Machine(bool[] targetLights, HashSet<int[]> buttons, int[] joltage, int id)
+{
+    public int Id = id;
+    public bool[] TargetLights = targetLights;
+    public HashSet<int[]> Buttons = buttons;
+    public int[] Joltage = joltage;
+
+    public static Machine FromInputString(string input, int index)
+    {
+        var parts = input.Split(' ');
+
+        bool[] targetLights = [.. parts[0].Where(c => c == '.' || c == '#').Select(c => c == '#')];
+        HashSet<int[]> buttons = GetButtonsFromInputString(parts[1..^1]);
+        int[] joltages = [.. parts[^1][1..^1].Split(',').Select(int.Parse)];
+
+        return new Machine(targetLights, buttons, joltages, index);
+    }
+
+    public int TryToSolveMachinePart1()
+    {
+        var initialState = new bool[TargetLights.Length];
+        var visited = new HashSet<bool[]>();
+        var queue = new Queue<(bool[] State, int Presses)>([(State: initialState, Presses: 0)]);
+
+        while (queue.Count > 0)
+        {
+            var (state, presses) = queue.Dequeue();
+            if (IsCorrectTargetLight(state))
+            {
+                return presses;
+            }
+
+            foreach (var button in Buttons)
+            {
+                var newState = ToggleButton(button, state);
+                if (visited.Add(newState))
+                {
+                    queue.Enqueue((newState, presses + 1));
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    public int TryToSolveMachinePart2()
+    {
+        var initialState = new int[Joltage.Length];
+        var visited = new HashSet<int[]>();
+        var queue = new Queue<(int[] State, int Presses)>([(State: initialState, Presses: 0)]);
+
+        while (queue.Count > 0)
+        {
+            var (state, presses) = queue.Dequeue();
+            if (IsCorrectJoltage(state))
+            {
+                return presses;
+            }
+
+            if (IsOverJoltage(state))
+            {
+                continue;
+            }
+
+            foreach (var button in Buttons)
+            {
+                var newState = ToggleSwitch(button, state);
+                if (visited.Add(newState))
+                {
+                    queue.Enqueue((newState, presses + 1));
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    private static int[] ToggleSwitch(int[] button, int[] state)
+    {
+        int[] newState = new int[state.Length];
+        Array.Copy(state, newState, state.Length);
+
+        for (int i = 0; i < button.Length; i++)
+        {
+            newState[button[i]] = newState[button[i]] + 1;
+        }
+        return newState;
+    }
+
+    public static bool[] ToggleButton(int[] button, bool[] state)
+    {
+        bool[] newState = new bool[state.Length];
+        Array.Copy(state, newState, state.Length);
+
+        for (int i = 0; i < button.Length; i++)
+        {
+            newState[button[i]] = !state[button[i]];
+        }
+        return newState;
+    }
+
+    private static HashSet<int[]> GetButtonsFromInputString(string[] inputStrings)
+    {
+        var buttons = new HashSet<int[]>();
+
+        for (var i = 0; i < inputStrings.Length; i++)
+        {
+            var button = inputStrings[i][1..^1].Split(',').Select(int.Parse).ToArray();
+            buttons.Add(button);
+        }
+
+        return buttons;
+    }
+
+    private bool IsCorrectTargetLight(bool[] state)
+    {
+        for (int i = 0; i < TargetLights.Length; i++)
+        {
+            if (targetLights[i] != state[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private bool IsCorrectJoltage(int[] state)
+    {
+        for (int i = 0; i < Joltage.Length; i++)
+        {
+            if (Joltage[i] != state[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private bool IsOverJoltage(int[] state)
+    {
+        for (int i = 0; i < Joltage.Length; i++)
+        {
+            if (Joltage[i] < state[i])
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ Solutions/Day10/Program.cs
@@ -0,0 +1,41 @@
+﻿using AocHelpers;
+using Day10;
+using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+
+Console.WriteLine("Day 10!");
+
+var inputs = InputFileReader.ReadInputFileToObjects<string>("./input.txt", "\n") ?? throw new NotSupportedException();
+//var inputs = InputFileReader.ReadInputFileToObjects<string>("./inputExample.txt", "\r\n") ?? throw new NotSupportedException();
+
+Console.WriteLine($"Found {inputs.Count} inputs.");
+Console.WriteLine($"Start solving...");
+
+var machines = inputs
+    .Select((input, index) => Machine.FromInputString(input, index))
+    .ToList();
+
+//var resultsPart1 = new ConcurrentBag<int>();
+//Parallel.ForEach(machines, machine =>
+//{
+//    resultsPart1.Add(machine.TryToSolveMachinePart1());
+//});
+
+//var resultPart1 = resultsPart1.Sum();
+//Console.WriteLine($"result part one: {resultPart1}");
+
+var resultsPart2 = new ConcurrentBag<int>();
+Parallel.ForEach(machines, new ParallelOptions
+{
+    MaxDegreeOfParallelism = 4 // Limit to a maximum of 4 concurrent tasks
+}, machine =>
+{
+    resultsPart2.Add(machine.TryToSolveMachinePart2());
+});
+
+var resultPart2 = resultsPart2.Sum();
+
+Console.WriteLine($"result part two: {resultPart2}");
+Console.WriteLine("Done!");
+Console.ReadKey();
