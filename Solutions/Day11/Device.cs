fbdeb0e61951b6969b6379a50db2695c996c1d6e
joeryhuiden <joeryhuiden@users.noreply.github.com>
2025-12-11 10:16:48 -0500

    Day 11

---
+++ Solutions/Day11/Device.cs
@@ -0,0 +1,51 @@
+﻿namespace Day11;
+
+public class Device(string name)
+{
+    public string Name { get; set; } = name;
+    public List<Device> Outputs { get; set; } = [];
+
+    public List<string> OutputNames { get; set; } = [];
+
+    public static Device Parse(string input, int _)
+    {
+        var parts = input.Split(" ");
+        var deviceName = parts[0][..^1].Trim();
+        var device = new Device(deviceName);
+
+        if (parts.Length > 1)
+        {
+            for (int i = 1; i < parts.Length; i++)
+            {
+                device.OutputNames.Add(parts[i].Trim());
+            }
+        }
+
+        return device;
+    }
+
+    public void LookUpDevices(List<Device> devices)
+    {
+        foreach (var outputName in OutputNames)
+        {
+            var device = devices.Find(d => d.Name.Equals(outputName, StringComparison.OrdinalIgnoreCase)) ?? throw new Exception($"Device with name {outputName} not found.");
+            Outputs.Add(device);
+        }
+    }
+
+    public long CountAmountOfPathsTo(string deviceNameToFind)
+    {
+        if (Name.Equals(deviceNameToFind, StringComparison.OrdinalIgnoreCase))
+        {
+            return 1;
+        }
+
+        long totalPaths = 0;
+        foreach (var output in Outputs)
+        {
+            totalPaths += output.CountAmountOfPathsTo(deviceNameToFind);
+        }
+
+        return totalPaths;
+    }
+}
---
+++ Solutions/Day11/Program.cs
@@ -0,0 +1,45 @@
+﻿using AocHelpers;
+using Day11;
+
+Console.WriteLine("Day 11!");
+
+var inputs = InputFileReader.ReadInputFileToObjects<string>("./input.txt", "\n") ?? throw new NotSupportedException();
+//var inputs = InputFileReader.ReadInputFileToObjects<string>("./inputExample.txt", "\n") ?? throw new NotSupportedException();
+//var inputs = InputFileReader.ReadInputFileToObjects<string>("./inputExamplePart2.txt", "\n") ?? throw new NotSupportedException();
+
+var devices = inputs.Select(Device.Parse).ToList();
+devices.Add(new Device("out"));
+
+foreach (var device in devices)
+{
+    device.LookUpDevices(devices);
+}
+
+Console.WriteLine($"Found {devices.Count} Devices.");
+Console.WriteLine($"Start solving...");
+
+// Part 1
+//var startingDevicePart1 = devices.First(d => d.Name.Equals("you", StringComparison.OrdinalIgnoreCase));
+//var resultPart1 = startingDevicePart1.CountAmountOfPathsTo("out");
+var resultPart1 = 0;
+
+// Part 2
+var resultPart2 = CalculatePart2(devices);
+
+Console.WriteLine("Done!");
+Console.WriteLine($"result part one: {resultPart1}");
+Console.WriteLine($"result part two: {resultPart2}");
+Console.ReadKey();
+
+static long CalculatePart2(List<Device> devices)
+{
+    var startingDevicePart2 = devices.First(d => d.Name.Equals("svr", StringComparison.OrdinalIgnoreCase));
+    var passingPointFftPart2 = devices.First(d => d.Name.Equals("fft", StringComparison.OrdinalIgnoreCase));
+    var passingPointDacPart2 = devices.First(d => d.Name.Equals("dac", StringComparison.OrdinalIgnoreCase));
+
+    // Assume svr-fft-dac-out
+    var sequence1 = startingDevicePart2.CountAmountOfPathsTo("fft") * passingPointFftPart2.CountAmountOfPathsTo("dac") * passingPointDacPart2.CountAmountOfPathsTo("out");
+    // Assume svr-dac-fft-out
+    var sequence2 = startingDevicePart2.CountAmountOfPathsTo("dac") * passingPointDacPart2.CountAmountOfPathsTo("fft") * passingPointFftPart2.CountAmountOfPathsTo("out");
+    return sequence1 + sequence2;
+}
